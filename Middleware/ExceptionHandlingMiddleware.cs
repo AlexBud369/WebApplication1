f10e770cefe4c9f7c9edbe4490a5d2d004fcf832
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-11-23 20:57:21 +0300

    feat: add middleware

---
+++ Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using WebApplication1.Exceptions;
+
+namespace WebApplication1.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try{
+            await _next(context);
+        } catch (Exception ex) {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        var response = new
+        {
+            error = exception.Message,
+            stackTrace = exception.StackTrace
+        };
+
+        context.Response.StatusCode = exception switch
+        {
+            NotFoundException => (int)HttpStatusCode.NotFound,
+            BadRequestException => (int)HttpStatusCode.BadRequest,
+            _ => (int)HttpStatusCode.InternalServerError
+        };
+
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
