3ab5ef3fb64389df1d46e8a9fde579ec6fb5bfee
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-11-23 14:00:46 +0300

    refactor: configure project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using WebApplication1.Data;
+using WebApplication1.Repositories.Contracts;
+using WebApplication1.Repositories.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IDietRepository, DietRepository>();
+builder.Services.AddScoped<IMealRepository, MealRepository>();
+builder.Services.AddScoped<INutrientRepository, NutrientRepository>();
+builder.Services.AddScoped<IMealProductRepository, MealProductRepository>();
+builder.Services.AddScoped<IProductNutrientRepository, ProductNutrientRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+
